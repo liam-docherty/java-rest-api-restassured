52da2c0d1aae804c5f106fdb4cca442d399ba71c
liam.docherty <liam.docherty@solarwinds.com>
2019-09-24 19:47:38 +0100

    Print each property from POST test response

--- src/test/java/todoist/entities/ProjectResponse.java
+++ src/test/java/todoist/entities/ProjectResponse.java
@@ -5,31 +5,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 // TODO: Investigate @JsonPropertyOrder, looks like it specifies the expected order
 public class ProjectResponse extends Project {
 
-    private int id;
-    private int order;
-    // TODO: Remove
-    private String name;
-    private int commentCount;
+    private Number id;
+    private Number order;
+    private Number commentCount;
 
-    public int getId() {
+    public Number getId() {
         return id;
     }
 
-    public int getOrder() {
+    public Number getOrder() {
         return order;
     }
 
-    // TODO: Remove next 2 methods
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @JsonProperty("comment_count")
-    public int getCommentCount() {
+    public Number getCommentCount() {
         return commentCount;
     }
 
--- src/test/java/todoist/projects/Post.java
+++ src/test/java/todoist/projects/Post.java
@@ -5,6 +5,7 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import org.junit.Test;
 import todoist.entities.ProjectRequest;
+import todoist.entities.ProjectResponse;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.config.EncoderConfig.encoderConfig;
@@ -13,12 +14,14 @@ import static todoist.BaseTest.*;
 public class Post {
 
     @Test
-    public void createProject() {
+    public void createProjectSuccess() {
 
         // TODO: Common header code, remove duplication
         // TODO: Remove logging once happy with tests
         Header authorization = new Header("Authorization", getApiToken());
         ProjectRequest payload = new ProjectRequest("deleteme-serialization");
+
+        ProjectResponse response =
 
         given().
                 // This is required because the todoist API will reject the call if the 'charset=UTF-8' is appended
@@ -31,7 +34,14 @@ public class Post {
                 post(getProjectsEndpoint()).
         then().
                 log().body().
-                assertThat().statusCode(200);
+                assertThat().statusCode(200).
+                assertThat().contentType(ContentType.JSON).
+                extract().as(ProjectResponse.class);
+
+        System.out.println("ID = " + response.getId());
+        System.out.println("Name = " + response.getName());
+        System.out.println("Order = " + response.getOrder());
+        System.out.println("Comment Count = " + response.getCommentCount());
 
     }
 
