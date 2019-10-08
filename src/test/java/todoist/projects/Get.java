d94746206551b70a56045876361a0255886ff1cf
liam.docherty <liam.docherty@solarwinds.com>
2019-10-08 19:21:48 +0100

    Get all projects attempt using ArrayList

--- src/test/java/todoist/projects/Get.java
+++ src/test/java/todoist/projects/Get.java
@@ -4,7 +4,10 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.Test;
+import todoist.entities.Project;
 import todoist.entities.ProjectResponse;
+
+import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,17 +20,42 @@ public class Get {
     public void retrieveAllProjectsSuccess() {
 
         // TODO: Common header code, remove duplication
-        // TODO: Remove logging once happy with tests
         Header authorization = new Header("Authorization", getApiToken());
+
+        Response response =
 
         given().
                 header(authorization).
-                log().all().
         when().
-                get(getProjectsEndpoint()).
-        then().
-                log().body().
-                assertThat().statusCode(200);
+                get(getProjectsEndpoint());
+
+        // ProjectResponse[] responseBody = response.getBody().as(ProjectResponse[].class);
+        ArrayList<ProjectResponse> responseBody = new ArrayList<ProjectResponse>();
+        responseBody = response.getBody().as(responseBody.getClass());
+
+        // TODO: Replace all of these random guid generators with a method in base test
+        Response newProjectResponse = setupProject("Project " + java.util.UUID.randomUUID());
+        ProjectResponse newProject = newProjectResponse.getBody().as(ProjectResponse.class);
+        String id = newProject.getId().toString();
+
+        // TODO: Remove this duplicate GET call
+        responseBody.add(newProject);
+
+        Response response2 =
+
+                given().
+                        header(authorization).
+                when().
+                        get(getProjectsEndpoint());
+
+        ArrayList<ProjectResponse> responseBody2 = new ArrayList<ProjectResponse>();
+        responseBody2 = response2.getBody().as(responseBody2.getClass());
+
+        assertThat(response2.getStatusCode(), is(200));
+        assertThat(response2.contentType(), is(ContentType.JSON.toString()));
+        assertThat(responseBody2, is(responseBody));
+
+        teardownProject(id);
 
     }
 
